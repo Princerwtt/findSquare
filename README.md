969d605033244604a9b3a514d179145df94eac03
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2022-10-11 14:56:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fineSquare
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-r4mxzu)
---
+++ script.js
@@ -0,0 +1,6 @@
+function findSquare(num) {
+  return num * num;
+  
+}
+findSquare();
+console.log(findSquare(2))
